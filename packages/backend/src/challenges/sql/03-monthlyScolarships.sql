d7e8fd4d66e457a6835cadd85696fa76b230c647
Fred's GitHub <stopchemtrailsfred@gmail.com>
2025-12-16 08:40:48 +0100

    add new sql problem

---
+++ packages/backend/src/challenges/sql/03-monthlyScolarships.sql
@@ -0,0 +1,3 @@
+DROP TABLE IF EXISTS scholarships;
+CREATE TABLE scholarships (id number, scholarship number);
+INSERT INTO scholarships VALUES (1,12000), (2,18000), (3,24000), (4,15000), (5,21000), (6,13000);
