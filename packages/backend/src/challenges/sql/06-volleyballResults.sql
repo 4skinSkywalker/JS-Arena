15c839782596e30b57f1e10070e284f8f42e34a9
Federico Trotta <117639221+fredowashere@users.noreply.github.com>
2025-12-16 11:33:59 +0100

    add new sql challenge

---
+++ packages/backend/src/challenges/sql/06-volleyballResults.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS results;
+CREATE TABLE results (name text, country text, scored number, missed number, wins number);
+INSERT INTO results VALUES ('FC Tokyo', 'Japan', 26, 28, 1);
+INSERT INTO results VALUES ('Fujian', 'China', 24, 26, 0);
+INSERT INTO results VALUES ('Jesus Maria', 'Argentina', 25, 23, 3);
+INSERT INTO results VALUES ('University Blues', 'Australia', 16, 25, 2);
