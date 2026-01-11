f2ad6d4bedc1c2a1c89e7a37270daefce736bbeb
Fred's GitHub <stopchemtrailsfred@gmail.com>
2026-01-11 18:17:15 +0100

    add solution to 72

---
+++ packages/backend/src/challenges/sql/72-orderPrices.sql
@@ -0,0 +1,8 @@
+WITH orders_new AS (
+    SELECT *, UNNEST(STRING_TO_ARRAY(items, ';'))::INTEGER AS order_id
+    FROM orders
+)
+SELECT orn.id, buyer, SUM(price) AS total_price
+FROM orders_new AS orn
+JOIN item_prices AS ip ON orn.order_id = ip.id
+GROUP BY orn.id, buyer;
