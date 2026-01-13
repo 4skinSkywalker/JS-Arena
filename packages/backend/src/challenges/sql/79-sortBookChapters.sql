8e705d2669e36d1d10eac31261732897c6a3a5d6
Federico Trotta <117639221+fredowashere@users.noreply.github.com>
2026-01-13 15:47:24 +0100

    update 79 solution to not use plsql

--- packages/backend/src/challenges/sql/79-sortBookChapters.sql
+++ packages/backend/src/challenges/sql/79-sortBookChapters.sql
@@ -1,32 +1,39 @@
-CREATE OR REPLACE FUNCTION roman_to_decimal(roman TEXT)
-RETURNS INTEGER AS $$
-DECLARE
-    roman_decimal_map JSONB := '{"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}';
-    sum INTEGER := 0;
-    curr INTEGER;
-    next INTEGER;
-BEGIN
-    roman := UPPER(roman);
-    
-    FOR i IN 1 .. LENGTH(roman) LOOP
-        curr := (roman_decimal_map ->> SUBSTRING(roman, i, 1))::INTEGER;
-        
-        IF i < LENGTH(roman) THEN
-            next := (roman_decimal_map ->> SUBSTRING(roman, i + 1, 1))::INTEGER;
-        ELSE
-            next := NULL;
-        END IF;
-        
-        IF curr < next THEN
-            sum := sum - curr;
-        ELSE
-            sum := sum + curr;
-        END IF;
-    END LOOP;
-    
-    RETURN sum;
-END; $$ LANGUAGE plpgsql;
-
+WITH unnested AS (
+    SELECT
+        chapter_name,
+        UNNEST(STRING_TO_ARRAY(chapter_number, NULL)) AS symbol
+    FROM book_chapters
+),
+numbered AS (
+    SELECT *, ROW_NUMBER() OVER () AS rn
+    FROM unnested
+),
+roman_to_integer AS (
+    SELECT *
+    FROM (
+        VALUES 
+            ('I', 1),
+            ('V', 5),
+            ('X', 10),
+            ('L', 50),
+            ('C', 100),
+            ('D', 500),
+            ('M', 1000)
+    ) AS t(roman, value)
+),
+integer_chapter_number AS (
+    SELECT
+        t1.chapter_name,
+        SUM(
+            CASE
+                WHEN (SELECT value FROM roman_to_integer WHERE roman = t1.symbol) < (SELECT value FROM roman_to_integer WHERE roman = t2.symbol) THEN -1 * (SELECT value FROM roman_to_integer WHERE roman = t1.symbol)
+                ELSE (SELECT value FROM roman_to_integer WHERE roman = t1.symbol)
+            END
+        ) AS chapter_number
+    FROM numbered AS t1
+    LEFT JOIN numbered AS t2 ON t1.chapter_name = t2.chapter_name AND t1.rn+1 = t2.rn
+    GROUP BY t1.chapter_name
+)
 SELECT chapter_name
-FROM book_chapters
-ORDER BY roman_to_decimal(chapter_number);
+FROM integer_chapter_number
+ORDER BY chapter_number;
