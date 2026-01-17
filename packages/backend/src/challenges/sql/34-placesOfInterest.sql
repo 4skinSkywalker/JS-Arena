ed8b07fac2eb98bfbf33eb608779fdac37eaabea
Fred's GitHub <stopchemtrailsfred@gmail.com>
2026-01-17 15:56:16 +0100

    format solution

--- packages/backend/src/challenges/sql/34-placesOfInterest.sql
+++ packages/backend/src/challenges/sql/34-placesOfInterest.sql
@@ -1,30 +1,26 @@
 SELECT 
     DISTINCT country,
     SUM(
-        CASE
-            WHEN leisure_activity_type = 'Adventure park'
-            THEN number_of_places
+        CASE leisure_activity_type
+            WHEN 'Adventure park' THEN number_of_places
             ELSE 0
         END
     ) AS adventure_park,
     SUM(
-        CASE
-            WHEN leisure_activity_type = 'Golf'
-            THEN number_of_places
+        CASE leisure_activity_type
+            WHEN 'Golf' THEN number_of_places
             ELSE 0
         END
     ) AS golf,
     SUM(
-        CASE
-            WHEN leisure_activity_type = 'River cruise'
-            THEN number_of_places
+        CASE leisure_activity_type
+            WHEN 'River cruise' THEN number_of_places
             ELSE 0
         END
     ) AS river_cruise,
     SUM(
-        CASE
-            WHEN leisure_activity_type = 'Kart racing'
-            THEN number_of_places
+        CASE leisure_activity_type
+            WHEN 'Kart racing' THEN number_of_places
             ELSE 0
         END
     ) AS kart_racing
