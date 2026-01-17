57832b7d4c183b5c462d96a4e54d5ad4e0bf6dc2
Fred's GitHub <stopchemtrailsfred@gmail.com>
2026-01-17 10:42:14 +0100

    fix 24 solution

--- packages/backend/src/challenges/sql/24-websiteUsers.sql
+++ packages/backend/src/challenges/sql/24-websiteUsers.sql
@@ -1,8 +1,11 @@
 SELECT
-    CASE
-        WHEN COUNT(*) > 1 THEN CONCAT('There are ', COUNT(*), ' ', user_type, 's')
-        ELSE CONCAT('There is ', 1, ' ', user_type)
+    CASE count
+        WHEN 1 THEN 'There is ' || count || ' ' || user_type
+        ELSE 'There are ' || count || ' ' || user_type || 's'
     END AS stats
-FROM users
-GROUP BY user_type
-ORDER BY COUNT(*) DESC;
+FROM (
+    SELECT user_type, COUNT(*)
+    FROM users
+    GROUP BY user_type
+)
+ORDER BY count DESC, user_type;
