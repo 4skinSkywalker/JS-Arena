255b7957c2b6c07090c5715cf4d15efd913bbe04
Fred's GitHub <stopchemtrailsfred@gmail.com>
2026-01-18 17:36:21 +0100

    update 41 solution

--- packages/backend/src/challenges/sql/41-netIncome.sql
+++ packages/backend/src/challenges/sql/41-netIncome.sql
@@ -1,11 +1,10 @@
-SELECT
-    SUBSTRING(date FROM '^(\d{4})')::INTEGER AS year,
-    CASE
-        WHEN SUBSTRING(date FROM '^\d{4}-(\d{2})')::INTEGER < 4 THEN 1
-        WHEN SUBSTRING(date FROM '^\d{4}-(\d{2})')::INTEGER < 7 THEN 2
-        WHEN SUBSTRING(date FROM '^\d{4}-(\d{2})')::INTEGER < 10 THEN 3
-        ELSE 4
-    END AS quarter,
-    SUM(profit - loss) AS net_profit
-FROM accounting
-GROUP BY year, quarter;
+SELECT year, quarter, SUM(profit - loss) AS net_profit
+FROM (
+    SELECT
+        *,
+        EXTRACT(year FROM date::DATE)::INTEGER AS year,
+        CEIL(EXTRACT(month FROM date::DATE)::DOUBLE PRECISION / 3) AS quarter
+    FROM accounting
+)
+GROUP BY year, quarter
+ORDER BY year, quarter;
