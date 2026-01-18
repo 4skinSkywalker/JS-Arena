19b81c5f9e1bf24a6ffa5e061bf94533a1dbbc43
Fred's GitHub <stopchemtrailsfred@gmail.com>
2026-01-18 14:05:41 +0100

    update 38 solution

--- packages/backend/src/challenges/sql/38-importantEvents.sql
+++ packages/backend/src/challenges/sql/38-importantEvents.sql
@@ -1,6 +1,5 @@
 SELECT id, name, event_date, participants
-FROM (
-    SELECT *, EXTRACT(DOW FROM event_date::DATE -1) AS weekday
-    FROM events
-)
-ORDER BY weekday, participants DESC;
+FROM events
+ORDER BY
+    EXTRACT(DOW FROM event_date::DATE - 1),
+    participants DESC;
