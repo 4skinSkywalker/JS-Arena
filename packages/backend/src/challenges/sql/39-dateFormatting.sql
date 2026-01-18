e46f140d03de88ba485d7ba7222b101daf03fdd6
Fred's GitHub <stopchemtrailsfred@gmail.com>
2026-01-18 14:16:49 +0100

    add tips and update solution of 39

--- packages/backend/src/challenges/sql/39-dateFormatting.sql
+++ packages/backend/src/challenges/sql/39-dateFormatting.sql
@@ -1,2 +1,5 @@
-SELECT date_str::DATE::TEXT AS date_iso
-FROM documents;
+SELECT (m[1] || '-' || LPAD(m[2], 2, '0') || '-' || LPAD(m[3], 2, '0')) AS date_iso
+FROM (
+    SELECT REGEXP_MATCH(date_str, '(\d{4})-(\d\d?)-(\d\d?)') AS m
+    FROM documents
+);
