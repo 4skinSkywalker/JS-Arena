b1292f493aafd061017ab74f0227f0f86cce8cda
Federico Trotta <117639221+fredowashere@users.noreply.github.com>
2026-01-19 14:49:36 +0100

    update 53

--- packages/backend/src/challenges/sql/53-officeBranches.sql
+++ packages/backend/src/challenges/sql/53-officeBranches.sql
@@ -1,10 +1,7 @@
-SELECT
-    branch_id,
-    b.name,
-    CASE
-        WHEN bt.name ~ '-outdated$' THEN NULL 
-        ELSE bt.id
-    END AS branchtype_id
-FROM branches AS b
-JOIN branch_types AS bt ON b.branchtype_id = bt.id
+UPDATE branches
+SET branchtype_id = NULL
+WHERE branchtype_id IN (SELECT id FROM branch_types WHERE name ~ '-outdated$');
+
+SELECT *
+FROM branches
 ORDER BY branch_id;
