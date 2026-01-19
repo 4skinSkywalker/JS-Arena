47df7c066dbaa0944737a706344faa111978412a
Federico Trotta <117639221+fredowashere@users.noreply.github.com>
2026-01-19 14:15:34 +0100

    update 47 solution

--- packages/backend/src/challenges/sql/47-localCalendar.sql
+++ packages/backend/src/challenges/sql/47-localCalendar.sql
@@ -1,9 +1,12 @@
 SELECT
-    e.event_id,
-    CASE
-        WHEN s.hours = 24 THEN TO_CHAR(date::TIMESTAMP + (s.timeshift || ' minutes')::INTERVAL, 'YYYY-MM-DD HH24:MI')
-        ELSE TO_CHAR(date::TIMESTAMP + (s.timeshift || ' minutes')::INTERVAL, 'YYYY-MM-DD HH12:MI PM')
-    END AS formatted_date
-FROM events AS e
-JOIN settings AS s ON e.user_id = s.user_id
-ORDER BY e.event_id;
+    event_id,
+    TO_CHAR(
+        date::TIMESTAMP + (timeshift || ' minutes')::INTERVAL,
+        CASE hours
+            WHEN 24 THEN 'YYYY-MM-DD HH24:MI'
+            ELSE 'YYYY-MM-DD HH:MI AM'
+        END
+    ) AS formatted_date
+FROM events e
+LEFT JOIN settings s ON e.user_id = s.user_id
+ORDER BY event_id;
