e07a7586aa3d1724448016b0fecd8e0c6623e7a4
Federico Trotta <117639221+fredowashere@users.noreply.github.com>
2026-01-19 14:30:32 +0100

    fix 50 description

--- packages/backend/src/challenges/sql/50-coursesDistribution.sql
+++ packages/backend/src/challenges/sql/50-coursesDistribution.sql
@@ -1,17 +1,15 @@
 ALTER TABLE groupcourses
-ADD FOREIGN KEY (course_id)
-    REFERENCES courses(id) ON DELETE CASCADE;
+ADD FOREIGN KEY (course_id) REFERENCES courses(id) ON DELETE CASCADE;
 
 ALTER TABLE groupexams
-ADD FOREIGN KEY (course_id)
-    REFERENCES courses(id) ON DELETE CASCADE;
+ADD FOREIGN KEY (course_id) REFERENCES courses(id) ON DELETE CASCADE;
 
 DELETE FROM courses
 WHERE name ~ '-toremove$';
 
 SELECT group_id, course_id
-  FROM groupcourses
- UNION
+FROM groupcourses
+UNION 
 SELECT group_id, course_id
-  FROM groupexams
- ORDER BY group_id, course_id
+FROM groupexams
+ORDER BY group_id, course_id;
