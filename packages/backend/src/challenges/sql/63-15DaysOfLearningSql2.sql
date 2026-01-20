a3cd0ca9aa1bcb601f51723dc5b362ac66b96b52
Federico Trotta <117639221+fredowashere@users.noreply.github.com>
2026-01-20 18:08:06 +0100

    update 63 solution

--- packages/backend/src/challenges/sql/63-15DaysOfLearningSql2.sql
+++ packages/backend/src/challenges/sql/63-15DaysOfLearningSql2.sql
@@ -1,32 +1,20 @@
-WITH deduped AS (
-    SELECT date, hacker_id
+WITH hacker_id_dates_with_rank AS (
+    SELECT
+        date,
+        hacker_id,
+        DENSE_RANK() OVER (PARTITION BY hacker_id ORDER BY date) AS rn
     FROM Submissions
     GROUP BY date, hacker_id
-),
-numbered AS (
-    SELECT *, ROW_NUMBER() OVER (PARTITION BY hacker_id ORDER BY date) AS rn
-    FROM deduped
     ORDER BY date
 ),
-islands AS (
-    SELECT *, (date::DATE - (rn || ' days')::INTERVAL)::DATE::TEXT AS island
-    FROM numbered
-),
-dates AS (
-    SELECT DISTINCT date
-    FROM Submissions
-    ORDER BY date
-),
-first_island AS (
+island_keys AS (
     SELECT
+        date,
         hacker_id,
-        MIN(date::DATE) AS starting_date,
-        MAX(date::DATE) AS ending_date
-    FROM islands
-    GROUP BY hacker_id, island
-    HAVING MIN(date::DATE)::TEXT = (SELECT * FROM dates LIMIT 1)
+        (date::DATE - ((rn - 1) || ' day')::INTERVAL)::DATE::TEXT AS island_key
+    FROM hacker_id_dates_with_rank
 )
 SELECT date, COUNT(*) AS hackers
-FROM dates, first_island
-WHERE ending_date >= date::DATE
+FROM island_keys
+WHERE island_key = (SELECT MIN(date) FROM Submissions)
 GROUP BY date;
