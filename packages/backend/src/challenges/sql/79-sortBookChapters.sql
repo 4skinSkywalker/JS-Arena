0ea77ab41eab7221e885ef253098f5985303cdf3
Federico Trotta <117639221+fredowashere@users.noreply.github.com>
2026-01-23 19:41:41 +0100

    fix 79 solution

--- packages/backend/src/challenges/sql/79-sortBookChapters.sql
+++ packages/backend/src/challenges/sql/79-sortBookChapters.sql
@@ -13,31 +13,30 @@ WITH roman_arabic AS (
 SELECT chapter_name
 FROM (
     SELECT
-        chapter_name,
-        SUM(
-            CASE
-                WHEN roman >= next_roman OR next_roman IS NULL THEN 1
-                ELSE -1
-            END * roman
-        ) AS arabic 
+        *,
+        (SELECT arabic FROM roman_arabic WHERE roman = t.digit) AS value,
+        (SELECT arabic FROM roman_arabic WHERE roman = t.next_digit) AS next_value
     FROM (
         SELECT
             chapter_name,
-            (SELECT arabic FROM roman_arabic AS r WHERE r.roman = p.roman) AS roman,
-            (SELECT arabic FROM roman_arabic AS r WHERE r.roman = p.next_roman) AS next_roman
+            digit,
+            LEAD(digit) OVER (PARTITION BY chapter_name ORDER BY rn) AS next_digit
         FROM (
-            SELECT
-                chapter_name,
-                roman,
-                LEAD(roman) OVER (PARTITION BY chapter_name) AS next_roman
+            SELECT *, ROW_NUMBER() OVER () AS rn
             FROM (
                 SELECT
                     chapter_name,
-                    UNNEST(STRING_TO_ARRAY(chapter_number, NULL)) AS roman
+                    UNNEST(STRING_TO_ARRAY(chapter_number, NULL)) AS digit
                 FROM book_chapters
             )
-        ) AS p
-    )
-    GROUP BY chapter_name
+        )
+    ) AS t
 )
-ORDER BY arabic;
+GROUP BY chapter_name
+ORDER BY
+    SUM(
+        CASE
+            WHEN value >= next_value OR next_value IS NULL THEN 1
+            ELSE -1
+        END * value
+    );
