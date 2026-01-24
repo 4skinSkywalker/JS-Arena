9c4bffbf592233d4c6d94105db0974cae7406698
Federico Trotta <117639221+fredowashere@users.noreply.github.com>
2026-01-24 14:36:57 +0100

    update 82 solution

--- packages/backend/src/challenges/sql/82-tictactoeTournament.sql
+++ packages/backend/src/challenges/sql/82-tictactoeTournament.sql
@@ -1,45 +1,49 @@
-WITH games AS (
+WITH stats AS (
     SELECT
-        *,
-        CASE 
-            COALESCE(SUBSTRING(board_fmt, '(O|X)\1\1'), '') ||
-            COALESCE(SUBSTRING(board_fmt, '(O|X).._\1.._\1..'), '') ||
-            COALESCE(SUBSTRING(board_fmt, '.(O|X)._.\1._.\1.'), '') ||
-            COALESCE(SUBSTRING(board_fmt, '..(O|X)_..\1_..\1'), '') ||
-            COALESCE(SUBSTRING(board_fmt, '..(O|X)_.\1._\1..'), '') ||
-            COALESCE(SUBSTRING(board_fmt, '(O|X).._.\1._..\1'), '')
+        o,
+        x,
+        CASE (
+            COALESCE(SUBSTRING(board, '(O|X)\1\1'), '') ||
+            COALESCE(SUBSTRING(board, '^(O|X)..-\1..-\1..'), '') ||
+            COALESCE(SUBSTRING(board, '^.(O|X).-.\1.-.\1.'), '') ||
+            COALESCE(SUBSTRING(board, '^..(O|X)-..\1-..\1'), '') ||
+            COALESCE(SUBSTRING(board, '^..(O|X)-.\1.-\1..'), '') ||
+            COALESCE(SUBSTRING(board, '^(O|X)..-.\1.-..\1'), '')
+        )
             WHEN 'X' THEN x
             WHEN 'O' THEN o
-            ELSE NULL
         END AS winner
     FROM (
         SELECT
             name_naughts AS o,
             name_crosses AS x,
-            STRING_AGG(line, '_') as board_fmt
+            STRING_AGG(match, '-') AS board
         FROM (
-            SELECT *, (REGEXP_MATCHES(board, '...', 'g'))[1] AS line
+            SELECT *, (REGEXP_MATCHES(board, '(.{3})', 'g'))[1] AS match
             FROM results
         )
-        GROUP BY timestamp, board, name_naughts, name_crosses
+        GROUP BY timestamp, name_naughts, name_crosses
     )
 ),
-player_stats AS (
+names AS (
+    SELECT o AS name FROM stats
+    UNION
+    SELECT x AS name FROM stats
+)
+SELECT *, (won * 2 + draw) AS points
+FROM (
     SELECT
         name,
-        SUM(CASE WHEN winner = name THEN 1 ELSE 0 END) AS won,
+        COUNT(*) AS played,
+        SUM(CASE WHEN name = winner THEN 1 ELSE 0 END) AS won,
         SUM(CASE WHEN winner IS NULL THEN 1 ELSE 0 END) AS draw,
-        SUM(CASE WHEN winner <> name THEN 1 ELSE 0 END) AS lost,
-        COUNT(*) AS played
-    FROM (
-        SELECT o AS name, winner
-        FROM games
-        UNION ALL
-        SELECT x AS name, winner
-        FROM games
-    ) AS player_games
+        SUM(CASE WHEN winner != name THEN 1 ELSE 0 END) AS lost
+    FROM names AS n
+    JOIN stats AS s ON n.name = s.o OR n.name = s.x
     GROUP BY name
 )
-SELECT *, (2 * won + draw) AS points
-FROM player_stats
-ORDER BY points DESC, played, won DESC, name;
+ORDER BY
+    points DESC,
+    played,
+    won DESC,
+    name;
