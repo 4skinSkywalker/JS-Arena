9245ea09b4ec9766033637294f4c5ce2b8ceabb0
Federico Trotta <117639221+fredowashere@users.noreply.github.com>
2026-02-02 10:21:17 +0100

    deploy

--- packages/backend/src/challenges/js/TrappingRainWater.js
+++ packages/backend/src/challenges/js/TrappingRainWater.js
@@ -1,10 +1,29 @@
 function solution({height}) {
-    return (
-        m = Math.max(...height),
-        Array.from({ length: m }, () => Array.from({ length: height.length }))
-            .map((a, i) =>a.map((b, j) => (i < m-height[j]) ? ' ' : '#').join(""))
-            .reduce((a, b) => (a += b.replace(/(?<=#)[^#]+(?=#)/g, m => "w".repeat(m.length)) + "\n", a), "")
-    )
+    const m = Math.max(...height);
+    const matrix = Array.from({ length: m }, () => Array.from({ length: height.length }));
+    
+    // Build walls
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            matrix[i][j] = (i < m-height[j]) ? ' ' : '#';
+        }
+    }
+    
+    // Trap water
+    for (let i = 0; i < matrix.length; i++) {
+        let collectMode = false;
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (matrix[i][j] === '#' && !collectMode) {
+                collectMode = !collectMode;
+            }
+            if (matrix[i][j] === ' ' && collectMode) {
+                matrix[i][j] = 'w';
+            }
+        }
+    }
+    
+    // Join matrix
+    return matrix.map(x => x.join('')).join('\n');
 }
 
 module.exports = solution;
