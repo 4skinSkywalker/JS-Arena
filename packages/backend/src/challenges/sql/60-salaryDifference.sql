49390918dc51bf32c10a71fa1cc85ceed68fbd25
Federico Trotta <federico.trotta@grupposcai.it>
2026-06-03 12:45:29 +0200

    fix solution

--- packages/backend/src/challenges/sql/60-salaryDifference.sql
+++ packages/backend/src/challenges/sql/60-salaryDifference.sql
@@ -1,27 +1,3 @@
-WITH rank_sum_salaries AS (
-    SELECT rank, SUM(salary)
-    FROM (
-        SELECT salary, RANK() OVER (ORDER BY salary)
-        FROM employees
-    )
-    GROUP BY rank
-),
-min_sum AS (
-    SELECT sum AS min_sum
-    FROM rank_sum_salaries
-    ORDER BY rank
-    LIMIT 1
-),
-max_sum AS (
-    SELECT sum AS max_sum
-    FROM rank_sum_salaries
-    ORDER BY rank DESC
-    LIMIT 1
-)
-SELECT (max_sum - min_sum) AS difference
-FROM min_sum, max_sum;
-
--- Another possible solution
 WITH min_max_salaries AS (
     SELECT MIN(salary), MAX(salary)
     FROM employees
@@ -32,5 +8,37 @@ min_max_count AS (
         COUNT(CASE WHEN salary = max THEN 1 END) AS max_count
     FROM employees, min_max_salaries
 )
-SELECT (max_count * max - min_count * min) AS difference
+SELECT COALESCE((max_count * max - min_count * min), 0) AS difference
 FROM min_max_salaries, min_max_count;
+
+-- Another possible solution
+WITH rank_sal AS (
+    SELECT rank, SUM(salary)
+    FROM (
+        SELECT salary, RANK() OVER (ORDER BY salary)
+        FROM employees
+    )
+    GROUP BY rank
+),
+min_sum AS (
+    (
+        SELECT sum AS l_sum
+        FROM rank_sal
+        ORDER BY rank
+    )
+    UNION ALL
+    (SELECT * FROM (VALUES (0)) AS t(l_sum))
+    LIMIT 1
+),
+max_sum AS (
+    (
+        SELECT sum AS h_sum
+        FROM rank_sal
+        ORDER BY rank DESC
+    )
+    UNION ALL
+    (SELECT * FROM (VALUES (0)) AS t(h_sum))
+    LIMIT 1
+)
+SELECT (h_sum - l_sum) AS difference
+FROM min_sum, max_sum;
