100077aef4bbf4f2455b22ed8737c6da6f7930c7
Federico Trotta <federico.trotta@grupposcai.it>
2026-06-03 13:02:29 +0200

    add another solution to recentHires

--- packages/backend/src/challenges/sql/61-recentHires.sql
+++ packages/backend/src/challenges/sql/61-recentHires.sql
@@ -13,3 +13,34 @@ FROM (
 )
 WHERE rank < 6
 ORDER BY d, name;
+
+-- Another possible solution
+SELECT name
+FROM (
+    (
+        SELECT * FROM (
+            SELECT *, 0 AS ord
+            FROM pr_department
+            ORDER BY date_joined DESC
+            LIMIT 5
+        ) ORDER BY name
+    )
+    UNION ALL
+    (
+        SELECT * FROM (
+            SELECT *, 1 AS ord
+            FROM it_department
+            ORDER BY date_joined DESC
+            LIMIT 5
+        ) ORDER BY name
+    )
+    UNION ALL
+    (
+        SELECT * FROM (
+            SELECT *, 2 AS ord
+            FROM sales_department
+            ORDER BY date_joined DESC
+            LIMIT 5
+        ) ORDER BY name
+    )
+);
